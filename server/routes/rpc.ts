7b8d8400213f38e907c48b2d81ced7dc64b1a7b5
ytrsoft <1654486219@qq.com>
2025-02-26 19:17:24 +0800

    ws

--- server/routes/rpc.ts
+++ server/routes/rpc.ts
@@ -1,11 +1,37 @@
+
+import * as frida from 'frida'
+
+const code = `
+const setup = () => {
+  Java.perform(() => {
+    console.log('===setup===')
+  })
+}
+rpc.exports = {
+  setup
+}
+`
+
+const createRPC = async () => {
+  const device = await frida.getUsbDevice()
+  const session = await device.attach('MOMO陌陌')
+  const script = await session.createScript(code)
+  script.load()
+  return script.exports
+}
+
+
 export default defineWebSocketHandler({
   open() {
-    console.log('开启RPC')
+    console.log('open websocket')
+    createRPC().then((rpc) => {
+      console.log(rpc)
+    })
   },
   message(peer: any, message: any) {
-    peer.send('=> ' + message)
+    peer.send('message ' + message)
   },
   close() {
-    console.log('关闭RPC')
+    console.log('close websocket')
   }
 })
